a03f71772e95f721c9ae530c3be799039bfaa827
honhimW <honhimw@outlook.com>
2025-10-11 09:46:21 +0800

    ci: gradle build & gradle test

--- bindings/java/src/test/java/io/github/honhimw/uuid/bench/BenchmarkRunner.java
+++ bindings/java/src/test/java/io/github/honhimw/uuid/bench/BenchmarkRunner.java
@@ -1,9 +1,7 @@
 package io.github.honhimw.uuid.bench;
 
-import org.junit.jupiter.api.Test;
 import org.openjdk.jmh.results.format.ResultFormatType;
 import org.openjdk.jmh.runner.Runner;
-import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
@@ -14,8 +12,7 @@ import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 public class BenchmarkRunner {
 
-    @Test
-    void benchmark() throws RunnerException {
+    public static void main(String[] args) throws Exception {
         Options options = new OptionsBuilder()
             .include(Self.class.getSimpleName())
             .include(AlternativeJdk.class.getSimpleName())
